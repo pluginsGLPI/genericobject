1f6f614344479c18a65d8ca361f122b485a7c6f3
walid <walid@349b9182-4a13-0410-896f-e5e9767dd1b3>
2011-10-24 17:15:21 +0000

    see #3216 Update headers
    git-svn-id: https://forge.indepnet.net/svn/genericobject/trunk@207 349b9182-4a13-0410-896f-e5e9767dd1b3

--- fields/field.constant.php
+++ fields/field.constant.php
@@ -2,12 +2,12 @@
 /*
  This file is part of the genericobject plugin.
 
- Order plugin is free software; you can redistribute it and/or modify
+ Genericobject plugin is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- Order plugin is distributed in the hope that it will be useful,
+ Genericobject plugin is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
