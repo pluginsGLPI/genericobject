240ad3f7814cf7c97e746cb2c88cb58acf462f02
walid <walid@349b9182-4a13-0410-896f-e5e9767dd1b3>
2011-10-26 17:16:27 +0000

    see #3230 include es_AR translation (reported by Luis A. Uriarte)
    git-svn-id: https://forge.indepnet.net/svn/genericobject/trunk@223 349b9182-4a13-0410-896f-e5e9767dd1b3

---
+++ locales/es_AR.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ ----------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2008 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org/
+ ----------------------------------------------------------------------
+
+ LICENSE
+
+	This file is part of GLPI.
+
+    GLPI is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    GLPI is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GLPI; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ------------------------------------------------------------------------
+*/
+
+// Original Author of file: Walid NOUH
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+$LANG['genericobject']['title'][1]="Administración de Objetos";
+$LANG['genericobject']['title'][2]="Tipo de objetos";
+
+$LANG['genericobject']['common'][1]="Identificador interno";
+$LANG['genericobject']['common'][2]="Tipo";
+$LANG['genericobject']['common'][3]="Estado";
+$LANG['genericobject']['common'][4]="Ya existe un tipo de objeto con ese nombre";
+$LANG['genericobject']['common'][5] = "Tipo de objeto no existe";
+$LANG['genericobject']['common'][6] = "El tipo de objeto debe comenzar con una letra";
+$LANG['genericobject']['common'][7] = "Ir a la lista de objetos";
+
+$LANG['genericobject']['install'][0] = "Error: los directorios '/inc', '/head' and '/ajax' del plugin se deben poder escribir en el servidor web.";
+
+$LANG['genericobject']['profile'][0] = "Gestión de derechos";
+$LANG['genericobject']['profile'][1] = "Asociar tickets con este objeto";
+$LANG['genericobject']['profile'][2] = "Acceso a objeto";
+$LANG['genericobject']['profile'][3] = "Tipo no definido";
+
+$LANG['genericobject']['config'][1] = "Uso";
+$LANG['genericobject']['config'][2] = "Estado de objeto";
+$LANG['genericobject']['config'][3] = "Comportamiento";
+$LANG['genericobject']['config'][6] = "Tipos de objetos";
+$LANG['genericobject']['config'][7] = "Vista preliminar";
+$LANG['genericobject']['config'][8] = "Vista preliminar del Objeto";
+$LANG['genericobject']['config'][9] = "Etiqueta";
+$LANG['genericobject']['config'][10] = "Plugin Inyección de archivo";
+$LANG['genericobject']['config'][11] = "Plugin PDF";
+$LANG['genericobject']['config'][12] = "Plugin Orden de compras";
+$LANG['genericobject']['config'][13] = "Plugins desinstalación de items";
+$LANG['genericobject']['config'][14] = "Conexiones de red";
+$LANG['genericobject']['config'][15] = "Plugin generacion de numero de inventario";
+
+$LANG['genericobject']['fields'][1] = "Campos asociados con el objeto";
+$LANG['genericobject']['fields'][2] = "Nombre en DB";
+$LANG['genericobject']['fields'][3] = "Etiqueta";
+$LANG['genericobject']['fields'][4] = "Agregar un campo nuevo";
+$LANG['genericobject']['fields'][5] = "Campo(s) borrados completamente";
+$LANG['genericobject']['fields'][6] = "Campo agregado completamente";
+$LANG['genericobject']['fields'][9] = "Debe configurar derechos para ver vista previa";
+$LANG['genericobject']['fields'][10] = "URL";
+
+$LANG['genericobject']['fields'][30] = "Fecha de expiración";
+$LANG['genericobject']['fields'][31] = "fecha de creación";
+
+$LANG['genericobject']['links'][1] = "Otros enlaces de objeto";
+$LANG['genericobject']['links'][2] = "Items asociados";
