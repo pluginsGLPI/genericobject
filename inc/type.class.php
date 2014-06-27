20da01b580dd54f844aacf1bb6d88e425e078436
mtpl <mtpl@349b9182-4a13-0410-896f-e5e9767dd1b3>
2014-06-27 12:38:51 +0000

    fixed #4945 The object Name must be singular
    git-svn-id: https://forge.indepnet.net/svn/genericobject/trunk@311 349b9182-4a13-0410-896f-e5e9767dd1b3

--- inc/type.class.php
+++ inc/type.class.php
@@ -1244,6 +1244,9 @@ class PluginGenericobjectType extends CommonDBTM {
     * @return the filtered value
     */
    static function filterInput($value) {
+      //Itemtype must always be singular, otherwise it breaks when using GLPI's framework
+      $value = getSingular($value);
+      
       $search  = explode(",","ç,æ,œ,á,é,í,ó,ú,à,è,ì,ò,ù,ä,ë,ï,ö,ü,ÿ,â,ê,î,ô,û,å,e,i,ø,u");
       $replace = explode(",","c,ae,oe,a,e,i,o,u,a,e,i,o,u,a,e,i,o,u,y,a,e,i,o,u,a,e,i,o,u");
       $value   = str_replace(array(' ', '_', '-', '+', '|', '[', ']', '\'','"', '@', '&', '~', '#', '='),
