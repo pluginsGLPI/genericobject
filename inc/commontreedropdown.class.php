8187336220dd2e341a0f4875d097b658b8eb907a
Kevin Roy <kiniou@gmail.com>
2014-08-28 18:36:08 +0200

    disable some logDebug

--- inc/commontreedropdown.class.php
+++ inc/commontreedropdown.class.php
@@ -48,12 +48,12 @@ class PluginGenericobjectCommonTreeDropdown extends CommonTreeDropdown {
    }
 
    static function getFormURL($full=true) {
-      Toolbox::logDebug("PluginGenericobjectCommonTreeDropdown::getFormURL", get_parent_class(get_called_class()));
+      //Toolbox::logDebug("PluginGenericobjectCommonTreeDropdown::getFormURL", get_parent_class(get_called_class()));
       return Toolbox::getItemTypeFormURL(  get_parent_class(get_called_class()), $full) .
          "?itemtype=".get_called_class();
    }
    static function getSearchURL($full=true) {
-      Toolbox::logDebug("PluginGenericobjectCommonTreeDropdown::getSearchURL", get_parent_class(get_called_class()));
+      //Toolbox::logDebug("PluginGenericobjectCommonTreeDropdown::getSearchURL", get_parent_class(get_called_class()));
       return Toolbox::getItemTypeSearchURL( get_parent_class(get_called_class()) , $full) .
          "?itemtype=".get_called_class();
 
