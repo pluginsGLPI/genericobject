e0bca039ae5e5603a2f2705404930d6455cc799f
Kevin Roy <kiniou@gmail.com>
2014-08-28 19:08:25 +0200

    remove private on static dbfields

--- inc/singletonobjectfield.class.php
+++ inc/singletonobjectfield.class.php
@@ -37,7 +37,7 @@ if (!defined('GLPI_ROOT')) {
 
 class PluginGenericobjectSingletonObjectField {
    /// Items list
-   private static $_dbfields = array();
+   static $_dbfields = array();
 
    /**
     * Singleton to store DB fields definition
