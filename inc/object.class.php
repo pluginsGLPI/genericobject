874a797a8ce61773194f0faf6b046899693c8b28
Kevin Roy <kiniou@gmail.com>
2014-09-10 12:16:37 +0200

    fix global search behavior

--- inc/object.class.php
+++ inc/object.class.php
@@ -134,8 +134,11 @@ class PluginGenericobjectObject extends CommonDBTM {
             }
          }
          if ($item->canUseGlobalSearch()) {
-            if (!in_array($class, $CFG_GLPI['state_types'])) {
-               array_push($CFG_GLPI['state_types'], $class);
+            if (!in_array($class, $CFG_GLPI['asset_types'])) {
+               array_push($CFG_GLPI['asset_types'], $class);
+            }
+            if (!in_array($class, $CFG_GLPI['globalsearch_types'])) {
+               array_push($CFG_GLPI['globalsearch_types'], $class);
             }
          }
 
