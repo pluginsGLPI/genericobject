35092c32e9cfc710310f174fae2a6e3b3609ca53
Alexandre Delaunay <adelaunay@teclib.com>
2015-04-10 15:11:20 +0200

    add global search (top right) declaration

--- inc/object.class.php
+++ inc/object.class.php
@@ -140,6 +140,7 @@ class PluginGenericobjectObject extends CommonDBTM {
          if ($item->canUseGlobalSearch()) {
             if (!in_array($class, $CFG_GLPI['state_types'])) {
                array_push($CFG_GLPI['state_types'], $class);
+               array_push($CFG_GLPI['globalsearch_types'], $class);
             }
          }
          
