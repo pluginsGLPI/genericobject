ec8c84aa25b4ebb9b831b8ce7d82c3e1863adb85
Emmanuel Haguet <ehaguet@teclib.com>
2016-02-11 13:45:55 +0100

    Menu : in asset menu, don't show familly deleted

--- inc/object.class.php
+++ inc/object.class.php
@@ -1136,12 +1136,18 @@ class PluginGenericobjectObject extends CommonDBTM {
                $family_id = $type['plugin_genericobject_typefamilies_id'];
                $name      = Dropdown::getDropdownName("glpi_plugin_genericobject_typefamilies", $family_id, 0, false);
                $str_name  = strtolower($name);
-               $menu[$str_name]['title'] = Dropdown::getDropdownName("glpi_plugin_genericobject_typefamilies", $family_id);
-               $menu[$str_name]['page']  = '/plugins/genericobject/front/familylist.php?id='.$family_id;
-               $menu[$str_name]['options'][strtolower($itemtype)] = 
-                     array('title' => $type['itemtype']::getMenuName(), 
-                           'page'  => $itemtype::getSearchUrl(false), 
-                           'links' => $links);
+
+               $title = Dropdown::getDropdownName("glpi_plugin_genericobject_typefamilies", $family_id);
+
+               // Show only the family who existed
+               if ($title != '&nbsp;') {
+                  $menu[$str_name]['title'] = $title;
+                  $menu[$str_name]['page']  = '/plugins/genericobject/front/familylist.php?id='.$family_id;
+                  $menu[$str_name]['options'][strtolower($itemtype)] = 
+                        array('title' => $type['itemtype']::getMenuName(), 
+                              'page'  => $itemtype::getSearchUrl(false), 
+                              'links' => $links);
+               }
             } else {
                $menu[strtolower($itemtype)]= array(
                   'title' => $type['itemtype']::getMenuName(),
