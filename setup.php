4731d1b6bc6379c741153e59c76340c1d2898dae
Cédric Anne <cedric.anne@gmail.com>
2018-06-22 15:24:20 +0200

    Add 2.6.0 version definition

--- setup.php
+++ setup.php
@@ -35,7 +35,7 @@
  ----------------------------------------------------------------------
  */
 
-define ('PLUGIN_GENERICOBJECT_VERSION', '2.5.2');
+define ('PLUGIN_GENERICOBJECT_VERSION', '2.6.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_GENERICOBJECT_MIN_GLPI", "9.3");
