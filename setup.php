827ff220d5f50c12f456c9051a0ce29ca6d05f0a
Kevin HAINRY <39367117+keha35@users.noreply.github.com>
2021-01-14 09:11:34 +0100

    Update setup.php
    Co-authored-by: Cédric Anne <cedric.anne@gmail.com>

--- setup.php
+++ setup.php
@@ -35,7 +35,7 @@
  ----------------------------------------------------------------------
  */
 
-define ('PLUGIN_GENERICOBJECT_VERSION', '2.9.3');
+define ('PLUGIN_GENERICOBJECT_VERSION', '2.9.2');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_GENERICOBJECT_MIN_GLPI", "9.5");
