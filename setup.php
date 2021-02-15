de2f8a4134cbd68c7fbadaa4e25cd87c63aabcf9
Cédric Anne <cedric.anne@gmail.com>
2021-02-15 15:10:41 +0100

    Add 2.10.0 version definition

--- setup.php
+++ setup.php
@@ -35,7 +35,7 @@
  ----------------------------------------------------------------------
  */
 
-define ('PLUGIN_GENERICOBJECT_VERSION', '2.9.2');
+define ('PLUGIN_GENERICOBJECT_VERSION', '2.10.0');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_GENERICOBJECT_MIN_GLPI", "9.5");
