65bd60f81c53b9d264332b210fb4fd478c397c79
Cédric Anne <cedric.anne@gmail.com>
2022-01-31 14:05:49 +0100

    Drop check util available in UI

--- tools/check_classes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include "../../../inc/includes.php";
-
-error_reporting( E_ALL  );
-ini_set("display_errors", 'stderr');
-ini_set("log_errors", true);
-
-PluginGenericobjectType::checkClassAndFilesForItemType();
