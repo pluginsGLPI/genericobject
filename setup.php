6042d7c9326954c1b022687158ec84152b57f6ed
Cédric Anne <cedric.anne@gmail.com>
2022-02-02 16:08:16 +0100

    2.12.0-rc2

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_GENERICOBJECT_VERSION', '2.12.0-rc1');
+define ('PLUGIN_GENERICOBJECT_VERSION', '2.12.0-rc2');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_GENERICOBJECT_MIN_GLPI", "10.0.0");
