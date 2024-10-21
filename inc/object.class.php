243649628e382bcb2268bc683db80e1a3e2a8ee5
Stanislas <skita@teclib.com>
2024-10-21 08:21:54 +0200

    Fix(Core): remove uselss and non-functional injection (#396)

--- inc/object.class.php
+++ inc/object.class.php
@@ -247,10 +247,6 @@ class PluginGenericobjectObject extends CommonDBTM
                     array_push($CFG_GLPI['asset_types'], $class);
                 }
 
-                if (!in_array($class, $CFG_GLPI['state_types'])) {
-                    array_push($CFG_GLPI['state_types'], $class);
-                }
-
                 if (!in_array($class, $CFG_GLPI['globalsearch_types'])) {
                     array_push($CFG_GLPI['globalsearch_types'], $class);
                 }
