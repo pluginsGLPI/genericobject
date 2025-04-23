93157b26f72e89f7ce6acbaefc0d98c77144bf16
Stanislas <skita@teclib.com>
2025-04-23 16:32:05 +0200

    2.14.14

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_GENERICOBJECT_VERSION', '2.14.13');
+define('PLUGIN_GENERICOBJECT_VERSION', '2.14.14');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_GENERICOBJECT_MIN_GLPI", "10.0.0");
